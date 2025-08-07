67811e84b0351a9dc7c1abb52c73e01d010f4262
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-08-07 23:29:54 +0700

    Add Turkish, Simplified Chinese, and Traditional Chinese locales for yargs
    - Added Turkish translations for commands, options, and error messages in `tr.json`.
    - Added Simplified Chinese translations for commands, options, and error messages in `zh_CN.json`.
    - Added Traditional Chinese translations for commands, options, and error messages in `zh_TW.json`.
    - Updated yargs package to version 16.2.0 with new locales and improved structure.
    - Refactored yargs entry point to use a consolidated helpers file for better modularity.
    - Updated package.json and package-lock.json to reflect new dependencies and structure.

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-const {tulisPertanyaan, simpanContacs} = require("./contacts");
+//? Mengambil argument dari command line
+import yargs from "yargs";
+import { tulisPertanyaan, simpanContacts } from './contacts.js';
 
-const main = async () => {
-  const nama = await tulisPertanyaan("Masukkan Nama Anda : ");
-  const email = await tulisPertanyaan("Masukkan Email Anda : ");
-  const nomorHp = await tulisPertanyaan("Masukkan Nomor HP Anda : ");
+const argv = yargs(process.argv.slice(2)).command({
+  command: "add",
+  describe: "Menambahkan Contact baru",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    nomorHp: {
+      describe: "Nomor Handphone",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    simpanContacts(argv.nama, argv.email, argv.nomorHp);
+  },
+}).argv;
 
-  simpanContacs(nama, email, nomorHp);
-};
 
-main();
+//? With vanila js
+// const {tulisPertanyaan, simpanContacs} = require("./contacts");
+
+// const main = async () => {
+//   const nama = await tulisPertanyaan("Masukkan Nama Anda : ");
+//   const email = await tulisPertanyaan("Masukkan Email Anda : ");
+//   const nomorHp = await tulisPertanyaan("Masukkan Nomor HP Anda : ");
+
+//   simpanContacs(nama, email, nomorHp);
+// };
+
+// main();
--- contacts.js
+++ contacts.js
@@ -1,6 +1,10 @@
-const fs = require("node:fs");
-const readline = require("node:readline");
-const { stdin: input, stdout: output } = require("node:process");
+import fs from "node:fs";
+import readline from "node:readline";
+import { stdin as input, stdout as output } from "node:process";
+import chalk from "chalk";
+import validator from "validator";
+
+export { tulisPertanyaan, simpanContacts };
 
 const rl = readline.createInterface({ input, output });
 
@@ -24,18 +28,45 @@ const tulisPertanyaan = (pertanyaan) => {
   });
 };
 
-const simpanContacs = (nama, email, nomorHp) => {
+const simpanContacts = (nama, email, nomorHp) => {
   const contact = { nama, email, nomorHp };
   const fileBuffer = fs.readFileSync("data/contacts.json", "utf-8");
   const contacts = JSON.parse(fileBuffer);
+
+  //? cek duplikasi
+  const duplikatNama = contacts.find((contact) => contact.nama === nama);
+  const duplikatNomorHp = contacts.find(
+    (contact) => contact.nomorHp === nomorHp
+  );
+  if (duplikatNama) {
+    console.log(chalk.red.bold("Nama sudah terdaftar, gunakan Nama lain"));
+    rl.close();
+    return false;
+  } else if (duplikatNomorHp) {
+    console.log(chalk.red.bold("Nomor sudah terdaftar, gunakan Nomor lain"));
+    rl.close();
+    return false;
+  }
+
+  //? cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.bold("Email tidak valid"));
+      rl.close();
+      return false;
+    }
+  }
+
+  //? cek NomorHp
+  if (!validator.isMobilePhone(nomorHp, 'id-ID')) {
+    console.log(chalk.red.bold("Nomor Handphone tidak valid"));
+    rl.close();
+    return false;
+  }
+
   contacts.push(contact);
 
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-  console.log("Terimakasi sudah memasukkan data");
+  console.log(chalk.green.bold("Pendaftaran Berhasil!!"));
   rl.close();
-};
-
-module.exports = {
-  tulisPertanyaan,
-  simpanContacs,
 };
