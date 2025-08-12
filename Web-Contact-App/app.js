b4d5d417f48fd568ce61ed9ef1ecb73845dee6fc
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-08-12 22:33:14 +0700

    make a logic whit package flie system and refactoring file data

--- Web-Contact-App/app.js
+++ Web-Contact-App/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const { loadContact, findContact } = require("./utils/contacts");
+const {
+  loadContact,
+  findContact,
+  addContact,
+  cekDuplikat,
+} = require("./utils/contacts");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
 
 const app = express();
 const port = 3000;
@@ -10,9 +18,23 @@ app.set("view engine", "ejs");
 
 //? Third-party Middleware
 app.use(expressLayouts);
+const { body, validationResult, check } = require("express-validator");
+
+//? Konfigurasi
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 //? Build-in Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const mahasiswa = [
@@ -50,9 +72,52 @@ app.get("/contact", (req, res) => {
     title: "Halaman Contact",
     layout: "layouts/main-layout",
     contacts,
+    msg: req.flash("msg"),
   });
 });
 
+//? Form tambah data contaact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "From Tambah Data Contact",
+    layout: "layouts/main-layout",
+  });
+});
+
+//? Proses data Contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom((value) => {
+      const duplikat = cekDuplikat(value);
+      if (duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email Tidak Valid!").isEmail(),
+    check("nomorHp", "Nomor Hp tidak Valid!").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(404).json({ errors: errors.array() });
+      res.render("add-contact", {
+        title: "From Tambah Data Contact",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+        old: req.body,
+      });
+    } else {
+      addContact(req.body);
+      //! Kirim flash message
+      req.flash("msg", "Data contact berhasi ditambahkan!");
+      res.redirect("/contact");
+    }
+  }
+);
+
+//? Halaman detail contact
 app.get("/contact/:nama", (req, res) => {
   const contact = findContact(req.params.nama);
   res.render("detail", {
--- Web-Contact-App/utils/contacts.js
+++ Web-Contact-App/utils/contacts.js
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 
-export { loadContact, findContact };
+export { loadContact, findContact, addContact, cekDuplikat };
 
 //? Membuat folder data jika belum ada
 const dirPath = "./data";
@@ -26,4 +26,22 @@ const findContact = (nama) => {
   const contacts = loadContact();
   const contact = contacts.find((contact) => contact.nama === nama);
   return contact;
+};
+
+//? Menulis file contacts json dengan data yang baru
+const saveContacts = (contacts) => {
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+};
+
+//? Menambahkan data contact baru
+const addContact = (contact) => {
+  const contacts = loadContact();
+  contacts.push(contact);
+  saveContacts(contacts);
+};
+
+//? cek nama yang duplikat
+const cekDuplikat = (nama) => {
+  const contacts = loadContact();
+  return contacts.find((contact) => contact.nama === nama);
 };
