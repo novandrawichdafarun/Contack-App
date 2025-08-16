2055f04e30bd7fa8a4a541c144e917dfa6f908bc
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-08-17 00:44:48 +0700

    add features Delete and Update Contact using method override and added package validators and flas massage

--- Mongo-Contact-App/app.js
+++ Mongo-Contact-App/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+const { body, validationResult, check, Result } = require("express-validator");
+const methodOverride = require("method-override");
+
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -10,6 +13,9 @@ const Contact = require("./model/contact");
 
 const app = express();
 const port = 3000;
+
+//* Setup method override
+app.use(methodOverride("_method"));
 
 //* Setup ejs
 app.set("view engine", "ejs");
@@ -72,6 +78,104 @@ app.get("/contact", async (req, res) => {
   });
 });
 
+//? Form tambah data contaact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "From Tambah Data Contact",
+    layout: "layouts/main-layout",
+  });
+});
+
+//? Proses tambah data Contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom(async (value) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email Tidak Valid!").isEmail(),
+    check("nomorHp", "Nomor Hp tidak Valid!").isMobilePhone("id-ID"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add-contact", {
+        title: "From Tambah Data Contact",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+        old: req.body,
+      });
+    } else {
+      await Contact.insertMany(req.body);
+      //! Kirim flash message
+      req.flash("msg", "Data contact berhasi ditambahkan!");
+      res.redirect("/contact");
+    }
+  }
+);
+
+//? Delete Contact
+app.delete("/contact", async (req, res) => {
+  await Contact.deleteOne({ nama: req.body.nama });
+  req.flash("msg", "Data contact berhasi dihapus!");
+  res.redirect("/contact");
+});
+
+//? Form ubah data contact
+app.get("/contact/edit/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+  res.render("edit-contact", {
+    title: "From Ubah Data Contact",
+    layout: "layouts/main-layout",
+    contact,
+  });
+});
+
+//? Prosess ubah data
+app.put(
+  "/contact",
+  [
+    body("nama").custom(async (value, { req }) => {
+      const duplikat = await Contact.findOne({ nama: value });
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email Tidak Valid!").isEmail(),
+    check("nomorHp", "Nomor Hp tidak Valid!").isMobilePhone("id-ID"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("edit-contact", {
+        title: "From Ubah Data Contact",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      await Contact.updateOne(
+        { _id: req.body._id },
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            nomorHp: req.body.nomorHp,
+          },
+        }
+      );
+      //! Kirim flash message
+      req.flash("msg", "Data contact berhasi diubah!");
+      res.redirect("/contact");
+    }
+  }
+);
+
 //? Halaman detail contact
 app.get("/contact/:nama", async (req, res) => {
   const contact = await Contact.findOne({ nama: req.params.nama });
@@ -83,12 +187,12 @@ app.get("/contact/:nama", async (req, res) => {
 });
 
 //? Menangani Jika Halaman web tidak ada
-app.use("/", (req, res) => {
-  res.status(404);
-  res.render("404page", {
-    title: "Error 404",
-  });
-});
+// app.use("/", (req, res) => {
+//   res.status(404);
+//   res.render("404page", {
+//     title: "Error 404",
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Mongo Contact App | listening at http://localhost:${port}`);
